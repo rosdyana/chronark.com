64ff1d34cd028b9e720b4a2a5448a82aa04a3332
Rosdyana Kusuma <rosdyana.kusuma@gmail.com>
2025-02-24 19:06:33 +0800

    [add] more songs

--- app/musics/page.tsx
+++ app/musics/page.tsx
@@ -2,28 +2,33 @@ import { Navigation } from "../components/nav";
 
 export default function Musics() {
     const tracks = [
-        "https://open.spotify.com/embed/track/1NrvnTrcWpLDc1j38bhsMq?utm_source=generator&theme=0"
+        "https://open.spotify.com/embed/track/1NrvnTrcWpLDc1j38bhsMq?utm_source=generator&theme=0",
+        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2025559428&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=true",
+        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2025557772&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=true"
     ];
 
     return (
-        <div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+        <div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
             <Navigation />
-            <div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
-                {tracks.map((track, index) => (
-                    <iframe
-                        key={index}
-                        style={{ borderRadius: "12px", marginBottom: "16px" }}
-                        src={track}
-                        width="100%"
-                        height="152"
-                        frameBorder="0"
-                        allowFullScreen
-                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                        loading="lazy"
-                    ></iframe>
-                ))}
+            <div className="container flex flex-col items-center justify-center min-h-screen px-4 mx-auto">
+                <div className="bg-white rounded-lg p-4 w-full max-w-2xl">
+                    <h2 className="text-2xl font-bold mb-4">Music Playlist</h2>
+                    {tracks.map((track, index) => (
+                        <div key={index} className="mb-4">
+                            <iframe
+                                style={{ borderRadius: "12px" }}
+                                src={track}
+                                width="100%"
+                                height="152"
+                                frameBorder="0"
+                                allowFullScreen
+                                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                                loading="lazy"
+                            ></iframe>
+                        </div>
+                    ))}
+                </div>
             </div>
-
         </div>
     );
 }
